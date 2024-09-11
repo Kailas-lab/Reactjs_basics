5642d0f18a61156e5cfae48c079a1ff9932475f7
Kailas Manikam <kailashraina9@gmail.com>
2024-09-11 18:24:27 +0530

    react-test-files

---
+++ myapp/src/components/Apple.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Apple extends React.Component {
+    render() {
+        return (
+            <h2>i am from class component</h2>
+        )
+    }
+}
+export default Apple;
--- myapp/src/components/Cat.js
+++ myapp/src/components/Cat.js
@@ -1,10 +1,12 @@
 import Dog from './Dog';
+import Apple from './Apple';
 
 function Cat(){
     return (
       <div>
          <h2>i am cat</h2>
-         <Dog />          //we have to import dog component because cat doesnt know dog
+         <Dog /> 
+         <Apple/>         {/*we have to import dog component because cat doesnt know dog*/}
       </div>
     );
   }
