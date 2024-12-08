a9d01fa84b1909cf8849566db4bfe7dbd55746f9
Kailas Manikam <kailashraina9@gmail.com>
2024-12-08 11:22:56 +0530

    react files

--- axios/axios/app/urls.py
+++ axios/axios/app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import ProductListView
+from .views import product_list_view
 
 urlpatterns = [
-    path('products/', ProductListView.as_view(), name='product-list'),
+    path('products/', product_list_view, name='product-list'),
 ]
--- axios/axios/axios/settings.py
+++ axios/axios/axios/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'axios.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'product',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  # Use '127.0.0.1' if localhost doesn't work
+        'PORT': '3306',  # Default MySQL port
     }
 }
 
